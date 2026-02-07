d9a607e2f7cb2231d7ac7fd82ae672ba2d865f27
LiberSoft <info@libersoft.org>
2026-02-07 22:59:00 +0100

    Fix shared

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
@@ -14,6 +16,10 @@ const config = {
 		}),
 		prerender: {
 			handleHttpError: 'warn',
+		},
+		alias: {
+			'@libershare/shared': path.resolve(__dirname, '../shared/src/index.ts'),
+			'@libershare/shared/*': path.resolve(__dirname, '../shared/src/*'),
 		},
 	},
 };
