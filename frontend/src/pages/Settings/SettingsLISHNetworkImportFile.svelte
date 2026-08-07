bb72d6fea70caeb8792199f1889d347904d3a457
LuRy <lury@lury.cz>
2026-08-07 18:17:17 +0200

    refactor(frontend): derive import filters from compression extensions

--- frontend/src/pages/Settings/SettingsLISHNetworkImportFile.svelte
+++ frontend/src/pages/Settings/SettingsLISHNetworkImportFile.svelte
@@ -4,7 +4,7 @@
 	import { LAYOUT } from '../../scripts/navigationLayout.ts';
 	import { storageLISHnetPath } from '../../scripts/settings.ts';
 	import { api } from '../../scripts/api.ts';
-	import { type LISHNetworkDefinition } from '@shared';
+	import { withCompressionExtensions, type LISHNetworkDefinition } from '@shared';
 	import ImportFileForm from '../../components/Import/ImportFileForm.svelte';
 	import ImportOverwrite from './SettingsLISHNetworkImportOverwrite.svelte';
 	interface Props {
@@ -30,7 +30,7 @@
 	}
 </script>
 
-<ImportFileForm {areaID} {position} {onBack} defaultDirectory={$storageLISHnetPath} fileFilter={['*.lishnet', '*.lishnets', '*.json', '*.lishnet.gz', '*.lishnets.gz', '*.json.gz', '*.lishnet.gzip', '*.lishnets.gzip', '*.json.gzip']} fileFilterName={'LISHNET ' + $t('common.extensions')} filePathLabel={$t('settings.lishNetworkImport.filePath')} {parseFile} {parseJSON} onConfirmDone={handleConfirmDone}>
+<ImportFileForm {areaID} {position} {onBack} defaultDirectory={$storageLISHnetPath} fileFilter={withCompressionExtensions(['*.lishnet', '*.lishnets', '*.json'])} fileFilterName={'LISHNET ' + $t('common.extensions')} filePathLabel={$t('settings.lishNetworkImport.filePath')} {parseFile} {parseJSON} onConfirmDone={handleConfirmDone}>
 	{#snippet confirm({ data, onDone })}
 		<ImportOverwrite networks={data as LISHNetworkDefinition[]} {position} {onDone} />
 	{/snippet}
